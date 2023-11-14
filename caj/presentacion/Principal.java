f3c8eaf3fb55d62554f60aa3827baee255490fc7
sasukelin <141195098+sasukelin@users.noreply.github.com>
2023-11-14 15:53:40 -0500

    Add files via upload

---
+++ caj/presentacion/Principal.java
@@ -0,0 +1,21 @@
+package presentacion;
+
+import presentacion.RegistroUsuario.RegistroUsuarioForm;
+import presentacion.VentanaPrincipal.VentanaPrincipalForms;
+
+/**
+ * Clase Principal de la aplicación
+ */
+public class Principal {
+    /**
+     * Primer método en ser ejecutado.
+     * @param args Argumentos que recibe por consola.
+     *
+     * Complejidad Temporal: O(1) Complejidad Constante.
+     */
+    public static void main(String[] args) {
+        //CrearPersonaForm ejemploGridLayout = new CrearPersonaForm();
+        //VentanaPrincipalForms ventanaPrincipalForm = new VentanaPrincipalForms();
+        RegistroUsuarioForm v = new RegistroUsuarioForm();
+    }
+}
